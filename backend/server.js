5803c3c1c5700c77df7a9eae8ced7e151ad43bb0
AlexCalderon075 <alexcalderon25502@gmail.com>
2026-04-17 09:43:05 -0600

    Migración a Supabase

--- backend/server.js
+++ backend/server.js
@@ -118,19 +118,18 @@ app.post('/api/historial', async (req, res) => {
 
 
 // RUTA: Obtener el historial filtrado por usuario
-app.get('/api/historial/:nombre', async (req, res) => {
-    const nombreUsuario = req.params.nombre;
+app.get('/api/historial', async (req, res) => {
+    // Aquí es donde el servidor "lee" lo que enviaste en la URL
+    const nombreUsuario = req.query.usuario; 
 
     try {
-        // Añadimos el "WHERE usuario_nombre = $1" para que no vea lo de otros
         const result = await pool.query(
             'SELECT * FROM historial_calculos WHERE usuario_nombre = $1 ORDER BY id DESC', 
             [nombreUsuario]
         );
         res.json(result.rows);
     } catch (err) {
-        console.error("Error al obtener historial:", err.message);
-        res.status(500).json({ error: "No se pudo obtener tu historial" });
+        res.status(500).json({ error: err.message });
     }
 });
 
